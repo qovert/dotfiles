dc5cfa388b0d98e618fd1ac2673a1a34551e7ec2
Brian Harkreader <github@qovert.com>
2022-10-29 10:09:45 -0700

    Template mods

---
+++ templates/bash_script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    echo 'Usage: ./script.sh arg-one arg-two
