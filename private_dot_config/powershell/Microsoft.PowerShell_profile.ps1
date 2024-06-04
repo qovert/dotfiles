1e5ec7033bcc4acc9ab442281455c997577d7c27
brian h <git@qovert.com>
2024-06-03 21:44:27 -0700

    Update .zshrc Update .config/powershell/Microsoft.PowerShell_profile.ps1

--- private_dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ private_dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -72,7 +72,7 @@ function Get-MyRGs () {
 
 # Enable oh-my-posh for different OSes
 if ($IsMacOS -and (get-command oh-my-posh)) {
-  $brewPrefix = brew --prefix oh-my-posh
+  $brewPrefix = Invoke-Expression -Command 'brew --prefix oh-my-posh'
   oh-my-posh init pwsh --config "$brewPrefix/themes/negligible.omp.json"
 } elseif ($IsLinux -and (Get-Command oh-my-posh)) {
 	oh-my-posh init pwsh --config '~/.poshthemes/microverse-power.omp.json' | Invoke-Expression
